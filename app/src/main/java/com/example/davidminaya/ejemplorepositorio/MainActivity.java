ab3bf2bede06d24caa172818a12d6ab7ccd12e27
david-minaya <davidminaya04@gmail.com>
2017-04-20 21:13:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/davidminaya/ejemplorepositorio/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.davidminaya.ejemplorepositorio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
