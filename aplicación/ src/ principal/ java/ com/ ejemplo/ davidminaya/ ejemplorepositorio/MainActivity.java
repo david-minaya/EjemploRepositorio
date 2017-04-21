94e55fbd956571ebf1d3142274d0f095cf2fef91
david-minaya <Davidminaya04@gmail.com>
2017-04-21 04:53:15 -0800

    Prueba commit desde el celular
    Esto es una prueba

--- aplicación/ src/ principal/ java/ com/ ejemplo/ davidminaya/ ejemplorepositorio/MainActivity.java
+++ aplicación/ src/ principal/ java/ com/ ejemplo/ davidminaya/ ejemplorepositorio/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.davidminaya.ejemplorepositorio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// comprobando commit desde el celular.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
